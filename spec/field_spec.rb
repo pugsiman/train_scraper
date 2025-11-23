d0a4b88974bd7270db158f68a4940a548dc7d273
Aviv Levinsky <levinskyaviv@gmail.com>
2025-11-23 17:13:11 +0200

    add field spec

---
+++ spec/field_spec.rb
@@ -0,0 +1,32 @@
+require 'field'
+
+RSpec.describe Field do
+  let(:page_mock) { Mechanize::Page.new(URI('http://example.com/mock.html'), nil, File.read('spec/page_mock.html'), 200, Mechanize.new) }
+  subject { described_class.new(:some_field, :string, ->(_page) { ' mock ' }) }
+
+  describe '#parse!' do
+    it 'parses HTML element into the result attribute' do
+      expect(subject.parse!(page_mock)).to eq ' mock '
+    end
+  end
+
+  describe 'format!' do
+    it 'formats the parse result' do
+      expect(subject.format!).to(change { subject.result })
+    end
+  end
+
+  describe 'validate!' do
+    context 'when validator passes' do
+      it 'validates the parse result' do
+        expect(subject.validate!).not_to raise_error(described_class::InvalidResultError)
+      end
+    end
+
+    context 'when validator does not pass' do
+      it 'raises an error' do
+        expect(subject.validate!).to raise_error(described_class::InvalidResultError)
+      end
+    end
+  end
+end
