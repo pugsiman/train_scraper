806a2db8e24820a9a20cf0688c49ae3594b5de8f
Aviv Levinsky <levinskyaviv@gmail.com>
2025-11-23 17:44:37 +0200

    fix expectation syntax in field_spec

--- spec/field_spec.rb
+++ spec/field_spec.rb
@@ -1,5 +1,4 @@
 require 'field'
-require 'pry'
 require 'mechanize'
 
 RSpec.describe Field do
@@ -14,20 +13,23 @@ RSpec.describe Field do
 
   describe 'format!' do
     it 'formats the parse result' do
-      expect(subject.format!).to(change { subject.result })
+      subject.parse!(page_mock)
+      expect { subject.format! }.to(change { subject.result }.to('mock'))
     end
   end
 
   describe 'validate!' do
     context 'when validator passes' do
+      before { subject.parse!(page_mock) }
+
       it 'validates the parse result' do
-        expect(subject.validate!).not_to raise_error(described_class::InvalidResultError)
+        expect { subject.validate! }.not_to raise_error
       end
     end
 
     context 'when validator does not pass' do
       it 'raises an error' do
-        expect(subject.validate!).to raise_error(described_class::InvalidResultError)
+        expect { subject.validate! }.to raise_error(described_class::InvalidResultError)
       end
     end
   end
